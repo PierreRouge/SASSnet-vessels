ecdc4d3f6b5c33320b1aee53d174bd23f6fedae6
Pierre Rougé <rouge@eq2-cf-626.creatis.insa-lyon.fr>
2023-10-12 11:33:41 +0200

    change patch size

--- code/train_gan_sdfloss.py
+++ code/train_gan_sdfloss.py
@@ -72,7 +72,7 @@ torch.manual_seed(args.seed)
 torch.cuda.manual_seed(args.seed)
 
 num_classes = 2
-patch_size = (160, 160, 96)
+patch_size = (128, 128, 96)
 
 def get_current_consistency_weight(epoch):
     # Consistency ramp-up from https://arxiv.org/abs/1610.02242
